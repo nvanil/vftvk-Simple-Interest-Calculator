c7bf328d54f136cef0f70c6361327c23cd29f9f3
nvanil <38245231+nvanil@users.noreply.github.com>
2022-03-04 15:25:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+        principal.style.color="yellow";
     
     var pal = parseInt(principal);
     
     if ( pal > 0 )
     
     {
-    
+        
+        
     var rate = document.getElementById("rate").value;
+        rate.style.color="yellow";
+        
     var years = document.getElementById("years").value;
 
     var interest = principal * years * rate /100;
+        interest.style.color="yellow";
     
     var year = new Date().getFullYear()+parseInt(years);
+        year.style.color="yellow";
 
     document.getElementById("result").innerHTML= "If you deposit "+principal+
     ",\<br\> at an interest rate of "+rate+"%"+
