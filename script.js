027e254e1e67c851c3d9b6af0c480605a43151e1
nvanil <38245231+nvanil@users.noreply.github.com>
2022-03-04 14:42:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,12 +11,11 @@ function compute()
     
     var year = new Date().getFullYear()+parseInt(years);
 
-    if ( principal > 0 ) {
     document.getElementById("result").innerHTML= "If you deposit "+principal+
     ",\<br\> at an interest rate of "+rate+"%"+
     ",\<br\>You will receive an amount of "+amount +
     ",\<br\>in the year "+year+"\<br\>"
-    }
+    
     
 }
 
